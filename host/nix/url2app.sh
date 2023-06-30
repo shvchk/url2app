2b3065d98014a3f43a620a868fa0cd15f65d4545
Andrei Shevchuk <andrei@shevchuk.co>
2023-07-01 00:35:27 +0300

    Add more example handlers

--- host/nix/url2app.sh
+++ host/nix/url2app.sh
@@ -1,7 +1,21 @@
 #! /usr/bin/env sh
 
-# Clean incoming URL
+# Clean incoming URL of url2app:// prefix
 url="$(echo -n "$1" | sed 's|url2app://||')"
 
-# Do anything, e.g. send notification with URL
-notify-send "$url"
+case $url in
+  #*pdf)
+  #  okular "$url" & ;;
+
+  #*.avi|*mp4|*mkv|*mov|*webm|*.ts)
+  #  mpv "$url" & ;;
+
+  #*youtu.be/*|*youtube.com/watch*)
+  #  mpv "$url" & ;;
+
+  #*jpg|*jpeg|*png|*webp)
+  #  GIO_USE_VFS=gvfs gimp "$url" & ;;
+
+  *)
+    notify-send "Example url2app:// handler" "$url" & ;;
+esac
