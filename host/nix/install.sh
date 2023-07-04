e6dc63a158744cc984978280d36784936760d181
Andrei Shevchuk <andrei@shevchuk.co>
2023-07-04 17:53:28 +0300

    Simplify nix install script

--- host/nix/install.sh
+++ host/nix/install.sh
@@ -2,21 +2,20 @@
 
 set -euo pipefail
 
+name="url2app"
 repo_cdn="https://cdn.jsdelivr.net/gh/shvchk/url2app"
 
-desktop_file_name="url2app.desktop"
 desktop_file_dir="${XDG_DATA_HOME:-$HOME/.local/share}/applications"
-desktop_file_url="${repo_cdn}/host/nix/${desktop_file_name}"
+desktop_file_url="${repo_cdn}/host/nix/${name}.desktop"
 
-exec_file_name="url2app.sh"
 exec_file_dir="$HOME/.local/bin"
-exec_file_url="${repo_cdn}/host/nix/${exec_file_name}"
+exec_file_url="${repo_cdn}/host/nix/${name}.sh"
 
 wget -P "$desktop_file_dir" "$desktop_file_url"
 wget -P "$exec_file_dir" "$exec_file_url"
 
-chmod +x "${exec_file_dir}/$exec_file_name"
-xdg-mime default "${desktop_file_dir}/${desktop_file_name}" x-scheme-handler/x-url2app
+chmod +x "${exec_file_dir}/${name}.sh"
+xdg-mime default "${desktop_file_dir}/${name}.desktop" x-scheme-handler/x-$name
 update-desktop-database "$desktop_file_dir"
 
 [[ $PATH =~ $exec_file_dir ]] || echo "$exec_file_dir is not in \$PATH. Make sure to add it."
