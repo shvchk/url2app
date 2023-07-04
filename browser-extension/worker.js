e77bee8245945bffa587d09d7e8180ce45290a63
Andrei Shevchuk <andrei@shevchuk.co>
2023-07-04 20:39:48 +0300

    Refactor menus, add menu for media

--- browser-extension/worker.js
+++ browser-extension/worker.js
@@ -2,49 +2,56 @@ import './util/browser-polyfill.js';
 import {defaultPrefs} from './defaultPrefs.js';
 
 const prefs = defaultPrefs;
+const menus = {
+  'page': { // id suffix, used in contextMenus.create → id
+    'contexts': ['page'], // ContextType, as in contextMenus.create → contexts
+    'srcProp': 'pageUrl' // OnClickData, as in contextMenus.onClicked event info
+  },
+  'link': {
+    'contexts': ['link'],
+    'srcProp': 'linkUrl'
+  },
+  'media': {
+    'contexts': ['image', 'audio', 'video'],
+    'srcProp': 'srcUrl'
+  }
+}
 
 browser.storage.onChanged.addListener(updatedPrefs => {
   Object.keys(updatedPrefs).forEach(k => {
     prefs[k] = updatedPrefs[k].newValue;
   });
   
-  updateMenus(prefs);
+  updateMenus(menus, prefs);
 });
 
 // Prefs are only up-to-date on the first run. For all other needs call storage.local.get().then()
 const once = () => browser.storage.local.get(defaultPrefs).then(restoredPrefs => {
   Object.assign(prefs, restoredPrefs);
-  init(prefs);
+  createMenus(menus, prefs);
 });
 
 browser.runtime.onStartup.addListener(once);
 browser.runtime.onInstalled.addListener(once);
 
 
-function updateMenus(prefs) {
-  browser.contextMenus.update('url2app-link', {
-    'targetUrlPatterns': prefs.allowedUrlPatterns
-  });
-
-  browser.contextMenus.update('url2app-page', {
-    'documentUrlPatterns': prefs.allowedUrlPatterns
-  });
+function updateMenus(menus, prefs) {
+  for (const m in menus) {
+    browser.contextMenus.update(`url2app-${m}`, {
+      'targetUrlPatterns': prefs.allowedUrlPatterns
+    });
+  }
 }
 
-function init(prefs) {
-  browser.contextMenus.create({
-    'id': 'url2app-link',
-    'title': 'Open in app',
-    'contexts': ['link'],
-    'targetUrlPatterns': prefs.allowedUrlPatterns
-  });
-
-  browser.contextMenus.create({
-    'id': 'url2app-page',
-    'title': 'Open in app',
-    'contexts': ['page'],
-    'documentUrlPatterns': prefs.allowedUrlPatterns
-  });
+function createMenus(menus, prefs) {
+  for (const m in menus) {
+    browser.contextMenus.create({
+      'id': `url2app-${m}`,
+      'title': `Open ${m} in app`,
+      'contexts': menus[m].contexts,
+      'targetUrlPatterns': prefs.allowedUrlPatterns
+    });
+  }
 }
 
 browser.action.onClicked.addListener(function (i) {
@@ -57,6 +64,7 @@ browser.action.onClicked.addListener(function (i) {
 });
 
 browser.contextMenus.onClicked.addListener(link => {
-  const url = link.linkUrl || link.pageUrl;
+  const menuId = link.menuItemId.replace('url2app-', '');
+  const url = link[menus[menuId].srcProp];
   browser.tabs.update({ url: 'x-url2app://' + url });
 });
