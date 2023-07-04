07f52e56b27b70385a4a7912cc6009a8355c3725
Andrei Shevchuk <andrei@shevchuk.co>
2023-07-04 17:00:16 +0300

    Make default message the same on all platforms

--- host/windows/url2app.js
+++ host/windows/url2app.js
@@ -20,7 +20,7 @@ if (/.*pdf$/.test(url)) {
   //shell.Run('"C:\\Program Files\\GIMP 2\\bin\\gimp-2.10.exe" "' + url + '"');
 
 } else {
-  shell.Popup('Good news: x-url2app:// protocol handler is working fine!\n' +
+  shell.Popup('Good news! x-url2app:// protocol handler is working fine!\n' +
               '\n' +
               'This URL has no handler defined, though: ' + url + '\n' +
               'You can add one in ' + WScript.ScriptFullName, 0, 'url2app');
