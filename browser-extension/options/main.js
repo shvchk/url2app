a73c321eb52f4036b4a69496f82fb1ee1e8692ae
Andrei Shevchuk <andrei@shevchuk.co>
2023-07-07 03:59:44 +0300

    Use bootstrap.css for options page

--- browser-extension/options/main.js
+++ browser-extension/options/main.js
@@ -1,28 +1,33 @@
 import '../util/browser-polyfill.js';
 import { defaultPrefs, getPrefs } from '../prefs.js';
 
-const info = document.getElementById('info');
-
 function restore() {
-  getPrefs().then(prefs => {
+  return getPrefs().then(prefs => {
     document.getElementById('allowedUrlPatterns').value = prefs.allowedUrlPatterns.join('\n');
     document.getElementById('mediaTypeHintEnabled').checked = prefs.mediaTypeHintEnabled;
   });
 }
 
 function save() {
-  browser.storage.local.set({
+  return browser.storage.local.set({
     allowedUrlPatterns: document.getElementById('allowedUrlPatterns').value
       .split(/[,\n]/)
       .map(s => s.trim())
       .filter((h, i, l) => h && l.indexOf(h) === i),
     mediaTypeHintEnabled: document.getElementById('mediaTypeHintEnabled').checked,
-  }).then(() => {
-    info.textContent = 'Saved';
-    restore();
-    window.setTimeout(() => info.textContent = '', 1000);
   });
 }
 
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.setAttribute('data-bs-theme', 'dark')
+}
+
 document.addEventListener('DOMContentLoaded', restore);
-document.getElementById('save').addEventListener('click', save);
+document.getElementById('save').addEventListener('click', event => {
+  save().then(() => {
+    const origTargetText = event.target.textContent;
+    event.target.textContent = '👌 Saved';
+    restore();
+    window.setTimeout(() => event.target.textContent = origTargetText, 1000);
+  });
+});
---
+++ browser-extension/options/purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['index.html', 'main.js'],
+  css: ['../../misc/bootstrap.css'],
+  output: 'bootstrap.css',
+  fontFace: true,
+  keyframes: true,
+  variables: true,
+}
